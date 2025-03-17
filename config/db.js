3680fd17fab2aaf46ccd91b86f27416823353d07
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-03-17 15:10:32 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 
 // Sett opp MySQL-tilkobling
 const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST || 'localhost',
-    user: process.env.DATABASE_USER || 'backenduser',
-    password: process.env.DATABASE_PASSWORD || 'yourpassword',
-    database: process.env.DATABASE_NAME || 'inventory_db',
-    waitForConnections: true,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     connectionLimit: 10,
     queueLimit: 0
 });
