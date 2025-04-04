5dc8739d70f7a610f0239cfd067f232648b4503a
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-04-04 08:01:40 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const pool = require("../config/db");
 
-router.get('/', (req, res) => {
-    res.send('user endpoint');
+router.get('/', async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM users");
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 module.exports = router;
